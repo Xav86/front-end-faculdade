2e4d40ac690c01a7f82079839205f15155b8bae1
Gustavo Goncalves Dos Santos <gustavo.330123@alunosatc.edu.br>
2025-05-05 21:48:54 -0300

    removido importações sem relevancia e visto estilização

--- aula06 - 28-04-2025/first-project/src/app/app.component.ts
+++ aula06 - 28-04-2025/first-project/src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 import { ContadorComponent } from './components/contador/contador.component';
 import { MostradorComponent } from './components/mostrador/mostrador.component';
 import { TodoComponent } from './components/todo/todo.component';
-// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, ContadorComponent, MostradorComponent, TodoComponent],
+  imports: [ContadorComponent, MostradorComponent, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
